682ad05331817cb91fd8335f5df56e5c289b09ba
Dwi Sasongko S <ruckuus@gmail.com>
2015-07-13 00:57:49 +0800

    Another refactor, make it OOP-ized

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
 var exec = require('child_process').exec, child, rows, line, big_arr, li, header;
 
-function removeEmptyElements(arr) {
-  return arr.filter(function(e) {
-    return e.trim() != '';
-  });
-}
+function StatService() {};
 
-function mapHeaderData(keys, elements) {
-  if (keys.length != elements.length) {
-    console.log("Error");
+StatService.prototype = {
+  utilRemoveEmptyElements: function(arr) {
+    return arr.filter(function(e) {
+      return e.trim() != '';
+    });
+  },
+
+  utilMapHeaderData: function(keys, elements) {
+    if (keys.length != elements.length) {
+      console.log("Error");
+    }
+
+    var myHash = {};
+    for (var i = 0; i < keys.length; i++) {
+      myHash[keys[i]] = elements[i];
+    }
+
+    return myHash;
+  },
+
+  doExec: function(command, cb) {
+    child = exec(command, function(error, stdout, stderr) {
+        if (error) {
+          console.log("Command error!\n");
+          cb(error);
+        }
+
+        rows = stdout.toString();
+        line = rows.split('\n');
+
+        // Get header
+        header = line.shift();
+        header = header.split(' ');
+        header = StatService.prototype.utilRemoveEmptyElements(header);
+
+        // Remove the last element if it's empty
+        if (!line[line.length]) {
+          line.pop();
+        }
+
+        big_arr = [];
+
+        line.forEach(function(item) {
+          li = item.split(' ');
+          li = StatService.prototype.utilRemoveEmptyElements(li);
+
+          big_arr.push(StatService.prototype.utilMapHeaderData(header, li));
+
+        });
+
+        cb(error, big_arr);
+     });
   }
-
-  var myHash = {};
-  for (var i = 0; i < keys.length; i++) {
-    myHash[keys[i]] = elements[i];
-  }
-
-  return myHash;
 }
 
-function doIt(command, cb) {
-  child = exec(command, function(error, stdout, stderr) {
-      if (error) {
-        console.log("Command error!\n");
-        cb(error);
-      }
-
-      rows = stdout.toString();
-      line = rows.split('\n');
-
-      // Get header
-      header = line.shift();
-      header = header.split(' ');
-      header = removeEmptyElements(header);
-
-      // Remove the last element if it's empty
-      if (!line[line.length]) {
-        line.pop();
-      }
-
-      big_arr = [];
-
-      line.forEach(function(item) {
-        li = item.split(' ');
-        li = removeEmptyElements(li);
-
-        big_arr.push(mapHeaderData(header, li));
-
-      });
-
-      cb(error, big_arr);
-   });
+function newStatService() {
+  return new StatService();
 }
 
-module.exports = function(cb) {
-  doIt('ps -ef', cb);
+module.exports =  function(cb) {
+  var srv = newStatService();
+  srv.doExec('ps -ef', cb);
 }
