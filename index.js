3bfba99939ad22024f2e4f98f6e2cf1f1daeceef
Dwi Sasongko S <ruckuus@gmail.com>
2015-07-13 09:18:06 +0800

    Split main function into smaller ones.

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ StatService.prototype = {
     return myHash;
   },
 
+  parseHeader: function(lines) {
+    header = lines.shift();
+    header = header.split(' ');
+    header = StatService.prototype.removeEmptyElements(header);
+
+    return header;
+  },
+
+  parseOutput: function(textArr) {
+    rows = textArr.toString();
+    line = rows.split('\n');
+
+    // Get header
+    header = StatService.prototype.parseHeader(line);
+
+    // Remove the last element if it's empty
+    if (!line[line.length]) {
+      line.pop();
+    }
+
+    big_arr = [];
+
+    line.forEach(function(item) {
+      li = item.split(' ');
+      li = StatService.prototype.removeEmptyElements(li);
+
+      big_arr.push(StatService.prototype.mapHeaderData(header, li));
+    });
+
+    return big_arr;
+  },
+
   doExec: function(command, cb) {
     child = exec(command, function(error, stdout, stderr) {
         if (error) {
@@ -41,30 +73,7 @@ StatService.prototype = {
           cb(error);
         }
 
-        rows = stdout.toString();
-        line = rows.split('\n');
-
-        // Get header
-        header = line.shift();
-        header = header.split(' ');
-        header = StatService.prototype.removeEmptyElements(header);
-
-        // Remove the last element if it's empty
-        if (!line[line.length]) {
-          line.pop();
-        }
-
-        big_arr = [];
-
-        line.forEach(function(item) {
-          li = item.split(' ');
-          li = StatService.prototype.removeEmptyElements(li);
-
-          big_arr.push(StatService.prototype.mapHeaderData(header, li));
-
-        });
-
-        cb(error, big_arr);
+        cb(error, StatService.prototype.parseOutput(stdout));
      });
   }
 }
